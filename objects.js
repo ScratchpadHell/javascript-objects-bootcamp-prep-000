993e3ed2629e5f24b152fce8f24390ee12e72699
Jacques Bellisario <gypsyoak@yahoo.com>
2018-03-23 18:13:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = new Object({ JimiHendrix: 'Foxy Lady'});
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, obj, { [key]: value})
+  return Object.assign({}, obj, { [key]: value })
 }
